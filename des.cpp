f91c32040bfffefc4ee4c288afb4022fa827db87
JorgeSuc <jorgesm595@gmail.com>
2019-09-19 17:37:13 -0600

    Add files via upload

---
+++ des.cpp
@@ -0,0 +1,11 @@
+/*	Universidad del Valle de Guatemala
+  	Programacion de Microprocesadores
+	Proyecto de encriptacion DES
+	Jorge Suchite
+	Alexa Bravo 
+
+	programa encriptado DES
+	version 1.0
+	Creado 15/09/2019
+	
+*/
